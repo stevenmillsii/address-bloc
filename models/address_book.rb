08eac0a52ef9c38ee282927c740a597a8a2a0c57
stevenmills <samills0608@gmail.com>
2017-06-01 21:20:24 -0400

    assignment-4-models: completed

--- models/address_book.rb
+++ models/address_book.rb
@@ -20,4 +20,14 @@ require_relative 'entry'
 
       entries.insert(index, Entry.new(name, phone_number, email))
     end
+
+    def remove_entry(name, phone_number, email)
+
+      entries.each do |entry|
+        if name == entry.name && phone_number == entry.phone_number && email == entry.email
+          entries.delete(entry)
+          break
+        end
+      end
+    end
   end
