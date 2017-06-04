22e100269747fc0b4b75b79419d4eb095903f948
stevenmills <samills0608@gmail.com>
2017-06-04 16:34:59 -0400

    checkpoint-6-csv: completed

--- models/address_book.rb
+++ models/address_book.rb
@@ -1,4 +1,5 @@
 require_relative 'entry'
+require "csv"
 
   class AddressBook
     attr_reader :entries
@@ -21,13 +22,13 @@ require_relative 'entry'
       entries.insert(index, Entry.new(name, phone_number, email))
     end
 
-    def remove_entry(name, phone_number, email)
+    def import_from_csv(file_name)
+      csv_text = File.read(file_name)
+      csv = CSV.parse(csv_text, headers: true, skip_blanks: true)
 
-      entries.each do |entry|
-        if name == entry.name && phone_number == entry.phone_number && email == entry.email
-          entries.delete(entry)
-          bre
-        end
+      csv.each do |row|
+        row_hash = row.to_hash
+        add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
       end
     end
   end
