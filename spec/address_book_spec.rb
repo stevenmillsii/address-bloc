e5f0dc11c2720c2524c080698f09f1f77a0ea6fb
stevenmills <samills0608@gmail.com>
2017-06-04 16:54:42 -0400

    assignment-6-csv: completed

--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -83,4 +83,30 @@ RSpec.describe AddressBook do
       check_entry(entry_five, "Sussie", "555-555-2036", "sussie@blocmail.com")
     end
   end
+
+  describe "#import_from_csv" do
+    it "imports the correct number of entries" do
+      book.import_from_csv("entries_2.csv")
+      book_size = book.entries.size
+      expect(book_size).to eq 3
+    end
+
+    it "imports the 1st entry" do
+      book.import_from_csv("entries_2.csv")
+      entry_one = book.entries[0]
+      check_entry(entry_one, "Brittany", "704-905-9231", "bcorn3501@gmail.com")
+    end
+
+    it "imports the 2nd entry" do
+      book.import_from_csv("entries_2.csv")
+      entry_two = book.entries[1]
+      check_entry(entry_two, "Steven", "803-389-1645", "samills0608@gmail.com")
+    end
+
+    it "imports the 3rd entry" do
+      book.import_from_csv("entries_2.csv")
+      entry_three = book.entries[2]
+      check_entry(entry_three, "Stout", "803-555-5555", "stout@puppy.com")
+    end
+  end
 end
